a1dbdfe5583fca47eb957665932f58050b0441bc
polmechtech <ndakarton@gmail.com>
2026-08-28 09:24:26 +0200

    Przekieruj techniczny adres Vercel na polmech.tech

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "polmech11-tjvs.vercel.app" }],
+        destination: "https://polmech.tech/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
